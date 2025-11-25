fc675dc643444dc94e64f033c53845184be711c3
strangesesay <johnmsesay1999@gmail.com>
2025-11-25 05:45:22 +0000

    still working on the cashier-dashboard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './core/components/home/home.component';
+import { LoginComponent } from './core/components/login/login.component';
+import { CashierDashboardComponent } from './core/components/cashier-dashboard/cashier-dashboard.component';
 
 export const routes: Routes = [
-{path: "", component: HomeComponent}
+{path: "", component: HomeComponent},
+{path: "login", component: LoginComponent},
+{path: "cashier-dashboard", component: CashierDashboardComponent}
 ];
--- src/app/core/components/cashier-dashboard/cashier-dashboard.component.ts
+++ src/app/core/components/cashier-dashboard/cashier-dashboard.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "../navbar/navbar.component";
 
 @Component({
   selector: 'app-cashier-dashboard',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './cashier-dashboard.component.html',
   styleUrl: './cashier-dashboard.component.css'
 })
